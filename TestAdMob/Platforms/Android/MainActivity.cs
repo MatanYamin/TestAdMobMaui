d494ce27b5eca636c28b9d981c017d40fa0c8870
Matan Yamin <yamin2211@gmail.com>
2024-08-02 18:03:04 +0300

    Adding essentials stuff for plugin

--- TestAdMob/Platforms/Android/MainActivity.cs
+++ TestAdMob/Platforms/Android/MainActivity.cs
@@ -1,10 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-
+using Plugin.MauiMTAdmob;
 namespace TestAdMob;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        var appId = "APP_ID"; // Replace app id
+
+        CrossMauiMTAdmob.Current.Init(this, appId);
+
+    }
 }
